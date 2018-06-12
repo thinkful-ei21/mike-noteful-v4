7078b1454ac8363d9ebec004c144f8e946777bbb
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-12 09:16:34 -0700

    completed endpoints and field validation

--- routes/users.js
+++ routes/users.js
@@ -10,44 +10,117 @@ const router = express.Router();
 
 /* ========== POST/CREATE USER ========== */
 router.post('/', (req, res, next) => {
-  const {
-    fullname,
-    username,
-    password
-  } = req.body;
+  const { fullname, username, password } = req.body;
 
-  return User.find({
-    username
-  })
-    .count()
-    .then(count => {
-      if (count > 0) {
-        return Promise.reject({
-          code: 422,
-          reason: 'ValidationError',
-          message: 'Username already taken',
-          location: 'username'
-        });
+  // VALIDATION ====== username and password fields are required
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    const err = new Error(`Missing '${missingField}' in request body`);
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    })
+      .then(err => next(err));
+  }
+
+  // VALIDATION ====== fields are type string
+  const stringFields = ['username', 'password','fullname'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  // VALIDATION ====== username and password should not have leading or trailing whitespace
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  // VALIDATION ====== username is a minimum of 1 character
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  //return User.find({ username })
+    // .count()
+    // .then(count => {
+    //   if (count > 0) {
+    //     return Promise.reject({
+    //       code: 422,
+    //       reason: 'ValidationError',
+    //       message: 'Username already taken',
+    //       location: 'username'
+    //     });
+    //   }
+    User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
+    .then(user => {
+      return res.location(`api/users/${user.id}`).status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
       }
-      return User.hashPassword(password)
-        .then(digest => {
-          const newUser = {
-            username,
-            password: digest,
-            fullname
-          };
-          return User.create(newUser);
-        })
-        .then(user => {
-          return res.location(`api/users/${user.id}`).status(201).json(user);
-        })
-        .catch(err => {
-          if (err.code === 11000) {
-            err = new Error('The username already exists');
-            err.status = 400;
-          }
-          next(err);
-        });
+      next(err);
     });
 });
 
