dbd9fcdf2fa416e603ddb947919c47dbef6afea3
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-13 15:13:50 -0700

    verified folders and tags ids

--- routes/notes.js
+++ routes/notes.js
@@ -5,12 +5,55 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Note = require('../models/note');
+const Folder = require('../models/folder');
+const Tag = require('../models/tag');
 
 const router = express.Router();
 
 
 // Protect endpoints using JWT Strategy
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+
+function validateFolderId(folderId, userId) {
+  if (folderId === undefined) {
+    return Promise.resolve();
+  }
+  if (!mongoose.Types.ObjectId.isValid(folderId)) {
+    const err = new Error('The `folderId` is not valid');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+  return Folder.count({ _id: folderId, userId })
+    .then(count => {
+      if(count === 0) {
+        const err = new Error('The tags `id` is not valid');
+        err.status = 400;
+        return Promise.reject(err);
+      }
+    });
+}
+
+function validateTagIds(tags, userId) {
+  if (tags=== undefined) {
+    return Promise.resolve();
+  }
+  if(Array.isArray(tags)) {
+    const err = new Error('The `tags` must be an array');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+  return Tag.find( { $and: [{_id: { $in: tags }, userId }] })
+    .then(results => {
+      if(tags.length !== results.length) {
+        const err = new Error('The tags `id` is not valid');
+        err.status = 400;
+        return Promise.reject(err);
+      }
+    });
+}
+
+
+
 
 
 /* ========== GET/READ ALL ITEMS ========== */
@@ -77,6 +120,7 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
   const { title, content, folderId, tags = [] } = req.body;
   const userId = req.user.id;
+  const newNote = { title, content, folderId, tags, userId };
 
   /***** Never trust users - validate input *****/
   if (!title) {
@@ -85,30 +129,24 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
-  if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
-    const err = new Error('The `folderId` is not valid');
-    err.status = 400;
-    return next(err);
-  }
-
-  if (tags) {
-    tags.forEach((tag) => {
-      if (!mongoose.Types.ObjectId.isValid(tag)) {
-        const err = new Error('The tags `id` is not valid');
+  Promise.all([
+    validateFolderId(folderId, userId),
+    validateTagIds(tags, userId)
+  ])
+    .catch(err => {
+      if (err === 'InvalidFolder') {
+        err = new Error('The folder is not valid');
         err.status = 400;
-        return next(err);
       }
-    });
-  }
-
-  const newNote = { title, content, folderId, tags, userId };
-
-  Note.create(newNote)
+      if (err === 'InvalidTag') {
+        err = new Error('The tag is not valid');
+        err.status = 400;
+      }
+      next(err);
+    })
+    .then(() => Note.create(newNote))
     .then(result => {
-      res
-        .location(`${req.originalUrl}/${result.id}`)
-        .status(201)
-        .json(result);
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
     .catch(err => {
       next(err);
@@ -120,6 +158,7 @@ router.put('/:id', (req, res, next) => {
   const { id } = req.params;
   const { title, content, folderId, tags = [] } = req.body;
   const userId = req.user.id;
+  const updateNote = { title, content, folderId, tags };
 
   /***** Never trust users - validate input *****/
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -134,24 +173,22 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
-    const err = new Error('The `folderId` is not valid');
-    err.status = 400;
-    return next(err);
-  }
-
-  if (tags) {
-    const badIds = tags.filter((tag) => !mongoose.Types.ObjectId.isValid(tag));
-    if (badIds.length) {
-      const err = new Error('The tags `id` is not valid');
-      err.status = 400;
-      return next(err);
-    }
-  }
-
-  const updateNote = { title, content, folderId, tags };
-
-  Note.findOneAndUpdate({ _id: id, userId }, updateNote, { new: true })
+  Promise.all([
+    validateFolderId(folderId, userId),
+    validateTagIds(tags, userId)
+  ])
+    .catch(err => {
+      if (err === 'InvalidFolder') {
+        err = new Error('The folder is not valid');
+        err.status = 400;
+      }
+      if (err === 'InvalidTag') {
+        err = new Error('The tag is not valid');
+        err.status = 400;
+      }
+      next(err);
+    })
+    .then(() => Note.findOneAndUpdate({ _id: id, userId }, updateNote, { new: true }))
     .then(result => {
       if (result) {
         res.json(result);
